83fe4171f81d67b42d40a1335a285c74746fb9b8
Kazi Munshimun <anik1.bd@gmail.com>
2017-03-31 00:33:08 +0600

    carbon bond commit

--- 3dTutorial/3dTutorial/Molecules.m
+++ 3dTutorial/3dTutorial/Molecules.m
@@ -23,6 +23,27 @@
     [self nodeWithAtom:[Atoms hydrogenAtoms] molecule:methaneMolecule andPosition:SCNVector3Make(0, -4, 0)];
     [self nodeWithAtom:[Atoms hydrogenAtoms] molecule:methaneMolecule andPosition:SCNVector3Make(0, +4, 0)];
     
+    SCNNode *carbonCarbonBondNode1 = [self carbonBond];
+    carbonCarbonBondNode1.position = SCNVector3Make(+2, 0, 0);
+    carbonCarbonBondNode1.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [methaneMolecule addChildNode:carbonCarbonBondNode1];
+    
+    SCNNode *carbonCarbonBondNode2 = [self carbonBond];
+    carbonCarbonBondNode2.position = SCNVector3Make(-2, 0, 0);
+    carbonCarbonBondNode2.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [methaneMolecule addChildNode:carbonCarbonBondNode2];
+    
+    
+    SCNNode *carbonCarbonBondNode3 = [self carbonBond];
+    carbonCarbonBondNode3.position = SCNVector3Make(0, 2, 0);
+    //  carbonCarbonBondNode3.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [methaneMolecule addChildNode:carbonCarbonBondNode3];
+    
+    SCNNode *carbonCarbonBondNode4 = [self carbonBond];
+    carbonCarbonBondNode4.position = SCNVector3Make(0, -2, 0);
+    //  carbonCarbonBondNode4.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [methaneMolecule addChildNode:carbonCarbonBondNode4];
+    
     
     return methaneMolecule;
 }
@@ -35,17 +56,50 @@
     [self nodeWithAtom:[Atoms carbonAtoms] molecule:ethanolMolecule andPosition:SCNVector3Make(-4, 0, 0)];
     
     //connect 2 carbon atoms with scncylinder
-    SCNCylinder *bondCylinder = [SCNCylinder cylinderWithRadius:0.3 height:4];
-    bondCylinder.firstMaterial.diffuse.contents = [UIColor darkGrayColor];
-    bondCylinder.firstMaterial.specular.contents = [UIColor whiteColor];
-    SCNNode *carbonCarbonBondNode = [SCNNode nodeWithGeometry:bondCylinder];
-    carbonCarbonBondNode.position = SCNVector3Make(-2, 0, 0);
-    carbonCarbonBondNode.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
     
-    [ethanolMolecule addChildNode:carbonCarbonBondNode];
+    SCNNode *carbonCarbonBondNode1 = [self carbonBond];
+    carbonCarbonBondNode1.position = SCNVector3Make(-2, 0, 0);
+    carbonCarbonBondNode1.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode1];
+    
+    SCNNode *carbonCarbonBondNode2 = [self carbonBond];
+    carbonCarbonBondNode2.position = SCNVector3Make(-6, 0, 0);
+    carbonCarbonBondNode2.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode2];
+    
+    
+    SCNNode *carbonCarbonBondNode3 = [self carbonBond];
+    carbonCarbonBondNode3.position = SCNVector3Make(-4, 2, 0);
+  //  carbonCarbonBondNode3.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode3];
+    
+    SCNNode *carbonCarbonBondNode4 = [self carbonBond];
+    carbonCarbonBondNode4.position = SCNVector3Make(-4, -2, 0);
+  //  carbonCarbonBondNode4.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode4];
     
     //1 oxygen connected with first carbon
     [self nodeWithAtom:[Atoms oxygenAtoms] molecule:ethanolMolecule andPosition:SCNVector3Make(+4, 0, 0)];
+    
+    SCNNode *carbonCarbonBondNode5 = [self carbonBond];
+    carbonCarbonBondNode5.position = SCNVector3Make(+2, 0, 0);
+    carbonCarbonBondNode5.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode5];
+    
+    SCNNode *carbonCarbonBondNode6 = [self carbonBond];
+    carbonCarbonBondNode6.position = SCNVector3Make(0, 2, 0);
+    //  carbonCarbonBondNode3.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode6];
+    
+    SCNNode *carbonCarbonBondNode7 = [self carbonBond];
+    carbonCarbonBondNode7.position = SCNVector3Make(0, -2, 0);
+    //  carbonCarbonBondNode4.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode7];
+    
+    SCNNode *carbonCarbonBondNode8 = [self carbonBond];
+    carbonCarbonBondNode8.position = SCNVector3Make(+6, 0, 0);
+    carbonCarbonBondNode8.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ethanolMolecule addChildNode:carbonCarbonBondNode8];
     
     //
     [self nodeWithAtom:[Atoms hydrogenAtoms] molecule:ethanolMolecule andPosition:SCNVector3Make(-8, 0, 0)];
@@ -58,6 +112,15 @@
     [self nodeWithAtom:[Atoms hydrogenAtoms] molecule:ethanolMolecule andPosition:SCNVector3Make(+8, 0, 0)];
     
     return ethanolMolecule;
+}
+
++(SCNNode *) carbonBond{
+    SCNCylinder *bondCylinder = [SCNCylinder cylinderWithRadius:0.2 height:4];
+    bondCylinder.firstMaterial.diffuse.contents = [UIColor darkGrayColor];
+    bondCylinder.firstMaterial.specular.contents = [UIColor whiteColor];
+    SCNNode *carbonCarbonBondNode = [SCNNode nodeWithGeometry:bondCylinder];
+ //   carbonCarbonBondNode.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    return carbonCarbonBondNode;
 }
 
 +(SCNNode *) ptfeMolecule{
@@ -74,6 +137,43 @@
     //2 flurine connected to second carbon
     [self nodeWithAtom:[Atoms flurineAtoms] molecule:ptfeMolecule andPosition:SCNVector3Make(-4, +4, 0)];
     [self nodeWithAtom:[Atoms flurineAtoms] molecule:ptfeMolecule andPosition:SCNVector3Make(-4, -4, 0)];
+    
+    SCNNode *carbonCarbonBondNode1 = [self carbonBond];
+    carbonCarbonBondNode1.position = SCNVector3Make(+2, 0, 0);
+    carbonCarbonBondNode1.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode1];
+    
+    SCNNode *carbonCarbonBondNode2 = [self carbonBond];
+    carbonCarbonBondNode2.position = SCNVector3Make(-2, 0, 0);
+    carbonCarbonBondNode2.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode2];
+    
+    
+    SCNNode *carbonCarbonBondNode3 = [self carbonBond];
+    carbonCarbonBondNode3.position = SCNVector3Make(0, 2, 0);
+    //  carbonCarbonBondNode3.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode3];
+    
+    SCNNode *carbonCarbonBondNode4 = [self carbonBond];
+    carbonCarbonBondNode4.position = SCNVector3Make(0, -2, 0);
+    //  carbonCarbonBondNode4.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode4];
+    
+    
+    SCNNode *carbonCarbonBondNode5 = [self carbonBond];
+    carbonCarbonBondNode5.position = SCNVector3Make(-4, 2, 0);
+    //  carbonCarbonBondNode3.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode5];
+    
+    SCNNode *carbonCarbonBondNode6 = [self carbonBond];
+    carbonCarbonBondNode6.position = SCNVector3Make(-4, -2, 0);
+    //  carbonCarbonBondNode4.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode6];
+    
+    SCNNode *carbonCarbonBondNode7 = [self carbonBond];
+    carbonCarbonBondNode7.position = SCNVector3Make(-6, 0, 0);
+    carbonCarbonBondNode7.rotation = SCNVector4Make(0, 0, 1, M_PI/2);
+    [ptfeMolecule addChildNode:carbonCarbonBondNode7];
     
     return ptfeMolecule;
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SceneKitPractice
+
+Trying to write SceneKit Get Started tutorial found in Ray wenderlich in objective C. Also trying to add Carbon bond that are not shown in tutorial using SCNCylinder. Also added Polytetrafluoroethylene structure which was missing in original tutorial.
