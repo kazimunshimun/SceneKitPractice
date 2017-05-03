62c10f18f1fc72b7fb12ce4ef516c8a9db5b510f
kazimunshimun <anik1.bd@gmail.com>
2017-05-03 19:04:44 +0600

    try to add camera

--- TreasureWorld/TreasureWorld/Controllers/GameViewController.m
+++ TreasureWorld/TreasureWorld/Controllers/GameViewController.m
@@ -9,10 +9,28 @@
 #import "GameViewController.h"
 #import "GameWorldScene.h"
 
-@interface GameViewController()
+@interface GameViewController(){
+    
+    SCNNode *cameraOrbit;
+    SCNNode *cameraNode;
+    SCNCamera *camera;
+}
 @property (strong, nonatomic) IBOutlet SCNView *worldView;
 
 @end
+
+//HANDLE PAN CAMERA
+CGFloat lastWidthRatio = 0;
+CGFloat lastHeightRatio = 0.2;
+int fingersNeededToPan = 1;
+CGFloat maxWidthRatioRight = 0.2;
+CGFloat maxWidthRatioLeft = -0.2;
+CGFloat maxHeightRatioXDown = 0.02;
+CGFloat maxHeightRatioXUp = 0.4;
+
+//HANDLE PINCH CAMERA
+CGFloat pinchAttenuation = 20.0;  //1.0: very fast ---- 100.0 very slow
+int lastFingersNumber = 0;
 
 @implementation GameViewController
 
@@ -21,13 +39,24 @@
     [super viewDidLoad];
 
    // self.worldView.scene = [[GameWorldScene alloc] init];
+    
     SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/simple_island_5.scn"];
     self.worldView.scene = scene;
+    
+    //[self drawPiramid];
     // allows the user to manipulate the camera
-    self.worldView.allowsCameraControl = YES;
+    //self.worldView.allowsCameraControl = YES;
     
     // show statistics such as fps and timing information
     self.worldView.showsStatistics = YES;
+    [self createCameraNode];
+    
+    // add a tap gesture recognizer
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [self.worldView addGestureRecognizer:panGesture];
+    
+    //allows the user to manipulate the camera
+    self.worldView.allowsCameraControl = NO;  //not needed
     
     /*
     // create a new scene
@@ -83,6 +112,75 @@
     [gestureRecognizers addObjectsFromArray:scnView.gestureRecognizers];
     scnView.gestureRecognizers = gestureRecognizers;
      */
+}
+
+-(void) createCameraNode{
+    //Create a camera like Rickster said
+    camera = [[SCNCamera alloc] init];
+    camera.usesOrthographicProjection = true;
+    camera.orthographicScale = 9;
+    camera.zNear = 1;
+    camera.zFar = 100;
+    
+    cameraNode = [[SCNNode alloc] init];
+    cameraNode.position = SCNVector3Make(0, 0, 50);
+    cameraNode.camera = camera;
+    
+    cameraOrbit = [[SCNNode alloc] init];
+    [cameraOrbit addChildNode:cameraNode];
+    [self.worldView.scene.rootNode addChildNode:cameraOrbit];
+    
+    //initial camera setup
+    [cameraOrbit setEulerAngles:SCNVector3Make((CGFloat)(-M_PI) * lastHeightRatio, (-2 * M_PI) * lastWidthRatio, 1)];
+}
+
+- (void) handlePan:(UIPanGestureRecognizer*)gestureRecognize{
+    
+    NSUInteger numberOfTouches = gestureRecognize.numberOfTouches;
+    
+    CGPoint translation = [gestureRecognize translationInView:gestureRecognize.view];
+    float widthRatio = (float)translation.x / (float)gestureRecognize.view.frame.size.width + lastWidthRatio;
+    float heightRatio = (float)translation.y / (float)gestureRecognize.view.frame.size.height + lastHeightRatio;
+    
+    if (numberOfTouches==fingersNeededToPan) {
+        
+        //  HEIGHT constraints
+        if (heightRatio >= maxHeightRatioXUp ) {
+            heightRatio = maxHeightRatioXUp;
+        }
+        if (heightRatio <= maxHeightRatioXDown ) {
+            heightRatio = maxHeightRatioXDown;
+        }
+        
+        
+        //  WIDTH constraints
+        if(widthRatio >= maxWidthRatioRight) {
+            widthRatio = maxWidthRatioRight;
+        }
+        if(widthRatio <= maxWidthRatioLeft) {
+            widthRatio = maxWidthRatioLeft;
+        }
+        
+        [cameraOrbit setEulerAngles:SCNVector3Make((float)(-M_PI) * heightRatio, (float)(-2 * M_PI) * widthRatio, 1)];
+        //print("Height: \(round(heightRatio*100))")
+        //print("Width: \(round(widthRatio*100))")
+        
+        
+        //for final check on fingers number
+        lastFingersNumber = fingersNeededToPan;
+    }
+    
+    lastFingersNumber = (numberOfTouches>0 ? numberOfTouches : lastFingersNumber);
+    
+    if (gestureRecognize.state == UIGestureRecognizerStateEnded && lastFingersNumber==fingersNeededToPan) {
+        lastWidthRatio = widthRatio;
+        lastHeightRatio = heightRatio;
+    }
+    
+}
+
+- (void) handlePich:(UIPinchGestureRecognizer*)gestureRecognize{
+    
 }
 
 - (void) handleTap:(UIGestureRecognizer*)gestureRecognize
